91ca308a93f0deba9d33d0fd41ae527b0f8a723c
Willissarino <willissarinoakitam@gmail.com>
2022-03-27 19:17:19 +0800

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './nav/top-nav/top-nav.component';
+import { FooterComponent } from './nav/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavComponent
+    TopNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
