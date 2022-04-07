f7fd1ee5ae9355e1b75065af41ee36b49087b680
Willissarino <willissarinoakitam@gmail.com>
2022-04-07 09:16:00 +0800

    minor

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -5,32 +5,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-
   //public product = <ProductDetails[]>{};
   categoryTitle = this.route.snapshot.paramMap.get('cate_slug');
   productName = this.route.snapshot.paramMap.get('prod_slug');
 
   // For single set of data
-  product : any = [];
+  product: any = [];
 
-  constructor(
-    private service:ProductService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private service: ProductService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.getProductDetails();
   }
 
-  getProductDetails() : void {
-    const cate_slug = this.route.snapshot.paramMap.get('cate_slug') ?? "";
-    const prod_slug = this.route.snapshot.paramMap.get('prod_slug') ?? "";
-    this.service.viewProductDetails(cate_slug,prod_slug)
-      .subscribe(response => this.product = response);
-
+  getProductDetails(): void {
+    const cate_slug = this.route.snapshot.paramMap.get('cate_slug') ?? '';
+    const prod_slug = this.route.snapshot.paramMap.get('prod_slug') ?? '';
+    this.service
+      .viewProductDetails(cate_slug, prod_slug)
+      .subscribe((response) => (this.product = response));
   }
 
+
+  createRange() {
+    return new Array(parseInt(this.product.qty));
+  }
 }
